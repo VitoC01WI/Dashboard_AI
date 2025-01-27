0c284cbbafbe358266584137c93a0098bd7e6396
VitoC01WI <man.tuen.chan@hu-berlin.de>
2025-01-27 21:47:39 +0100

    Create app_copy.py

---
+++ app_copy.py
@@ -0,0 +1,276 @@
+import streamlit as st
+from openai import OpenAI
+
+
+#ai chatbot with data
+
+# Show title and description.
+st.title("📄 Document question answering")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via `st.text_area`.
+    question = st.text_area(
+        "Now ask a question about the document!",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
+
+
+#the GDP dashboard example
+
+import pandas as pd
+import math
+from pathlib import Path
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='GDP dashboard',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+)
+
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
+
+@st.cache_data
+def get_gdp_data():
+    """Grab GDP data from a CSV file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
+    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+
+    MIN_YEAR = 1960
+    MAX_YEAR = 2022
+
+    # The data above has columns like:
+    # - Country Name
+    # - Country Code
+    # - [Stuff I don't care about]
+    # - GDP for 1960
+    # - GDP for 1961
+    # - GDP for 1962
+    # - ...
+    # - GDP for 2022
+    #
+    # ...but I want this instead:
+    # - Country Name
+    # - Country Code
+    # - Year
+    # - GDP
+    #
+    # So let's pivot all those year-columns into two: Year and GDP
+    gdp_df = raw_gdp_df.melt(
+        ['Country Code'],
+        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+        'Year',
+        'GDP',
+    )
+
+    # Convert years from string to integers
+    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+
+    return gdp_df
+
+gdp_df = get_gdp_data()
+
+# -----------------------------------------------------------------------------
+# Draw the actual page
+
+# Set the title that appears at the top of the page.
+'''
+# :earth_americas: GDP dashboard
+
+Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
+notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
+But it's otherwise a great (and did I mention _free_?) source of data.
+'''
+
+# Add some spacing
+''
+''
+
+min_value = gdp_df['Year'].min()
+max_value = gdp_df['Year'].max()
+
+from_year, to_year = st.slider(
+    'Which years are you interested in?',
+    min_value=min_value,
+    max_value=max_value,
+    value=[min_value, max_value])
+
+countries = gdp_df['Country Code'].unique()
+
+if not len(countries):
+    st.warning("Select at least one country")
+
+selected_countries = st.multiselect(
+    'Which countries would you like to view?',
+    countries,
+    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+
+''
+''
+''
+
+# Filter the data
+filtered_gdp_df = gdp_df[
+    (gdp_df['Country Code'].isin(selected_countries))
+    & (gdp_df['Year'] <= to_year)
+    & (from_year <= gdp_df['Year'])
+]
+
+st.header('GDP over time', divider='gray')
+
+''
+
+st.line_chart(
+    filtered_gdp_df,
+    x='Year',
+    y='GDP',
+    color='Country Code',
+)
+
+''
+''
+
+
+first_year = gdp_df[gdp_df['Year'] == from_year]
+last_year = gdp_df[gdp_df['Year'] == to_year]
+
+st.header(f'GDP in {to_year}', divider='gray')
+
+''
+
+cols = st.columns(4)
+
+for i, country in enumerate(selected_countries):
+    col = cols[i % len(cols)]
+
+    with col:
+        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+
+        if math.isnan(first_gdp):
+            growth = 'n/a'
+            delta_color = 'off'
+        else:
+            growth = f'{last_gdp / first_gdp:,.2f}x'
+            delta_color = 'normal'
+
+        st.metric(
+            label=f'{country} GDP',
+            value=f'{last_gdp:,.0f}B',
+            delta=growth,
+            delta_color=delta_color
+        )
+
+##table example
+import altair as alt
+
+# Show the page title and description.
+st.set_page_config(page_title="Movies dataset", page_icon="🎬")
+st.title("🎬 Movies dataset")
+st.write(
+    """
+    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
+    It shows which movie genre performed best at the box office over the years. Just 
+    click on the widgets below to explore!
+    """
+)
+
+
+# Load the data from a CSV. We're caching this so it doesn't reload every time the app
+# reruns (e.g. if the user interacts with the widgets).
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/movies_genres_summary.csv")
+    return df
+
+
+df = load_data()
+
+# Show a multiselect widget with the genres using `st.multiselect`.
+genres = st.multiselect(
+    "Genres",
+    df.genre.unique(),
+    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+)
+
+# Show a slider widget with the years using `st.slider`.
+years = st.slider("Years", 1986, 2006, (2000, 2016))
+
+# Filter the dataframe based on the widget input and reshape it.
+df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+df_reshaped = df_filtered.pivot_table(
+    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+)
+df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+
+
+# Display the data as a table using `st.dataframe`.
+st.dataframe(
+    df_reshaped,
+    use_container_width=True,
+    column_config={"year": st.column_config.TextColumn("Year")},
+)
+
+# Display the data as an Altair chart using `st.altair_chart`.
+df_chart = pd.melt(
+    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
+)
+chart = (
+    alt.Chart(df_chart)
+    .mark_line()
+    .encode(
+        x=alt.X("year:N", title="Year"),
+        y=alt.Y("gross:Q", title="Gross earnings ($)"),
+        color="genre:N",
+    )
+    .properties(height=320)
+)
+st.altair_chart(chart, use_container_width=True)
