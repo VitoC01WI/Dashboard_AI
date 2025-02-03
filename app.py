6f2063b79a05f9decaf68d508afcd6e182f7558f
Man Tuen Chan <chan1@uni-potsdam.de>
2025-02-03 22:28:27 +0100

    aspect

--- app.py
+++ app.py
@@ -40,18 +40,18 @@ product_data = pd.DataFrame({
     })
 
 #row 2
-c1, c2 = st.columns([4,2])#use list for ratio
-with c1.container(height=250):
+c1, c2 = st.columns([3,2])#use list for ratio
+with c1.container(height=260):
     st.markdown("Waiting time (in min) at cash desk")
-    st.bar_chart(time_data, x="Time", y="Waiting Time", height = 200)   
+    st.bar_chart(time_data, x="Time", y="Waiting Time", height = 220)   
     #fig, ax = plt.subplots(figsize=(10, 4))  # Define aspect ratio
     #ax.bar(time_data["Time"], time_data["Waiting Time"])
     #ax.set_xlabel("Time")
     #ax.set_ylabel("Waiting Time (min)")
     #st.pyplot(fig)
-with c2.container(height=250):
+with c2.container(height=260):
     st.markdown("Product Sales and Forecast")
-    st.line_chart(sales_data, x="Month", y="Sales", height = 200)
+    st.line_chart(sales_data, x="Month", y="Sales", height = 220)
     #fig, ax = plt.subplots() 
     #ax.plot(sales_data["Month"], sales_data["Sales"], marker='o')
     #ax.set_xlabel("Month")
@@ -60,19 +60,19 @@ with c2.container(height=250):
 
 #row 3
 
-c3, c4 = st.columns([4,2])
-with c3.container(height=250):
+c3, c4 = st.columns([3,2])
+with c3.container(height=260):
     st.markdown("Customers per Day")
-    st.bar_chart(customer_data, x="Time", y="Customers", height = 200)
+    st.bar_chart(customer_data, x="Time", y="Customers", height = 220)
     #fig, ax = plt.subplots(figsize=(10, 4))  # Define aspect ratio
     #ax.bar(customer_data["Time"], customer_data["Customers"])
     #ax.set_xlabel("Time")
     #ax.set_ylabel("Customers")
     #st.pyplot(fig)
 
-with c4.container(height=250):
+with c4.container(height=260):
     st.markdown("Product Variation List")
-    st.dataframe(product_data, height = 200)
+    st.dataframe(product_data, height = 220)
 
 #template leftover
 #    @st.cache_data
